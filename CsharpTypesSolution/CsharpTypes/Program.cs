ec6b2ecbb0853a943d8b5576872a979c35c8f12a
max-student <max-student@maxtrain.com>
2020-06-17 11:45:23 -0400

    Initial commit

---
+++ CsharpTypesSolution/CsharpTypes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CsharpTypes {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Welcome to the world's simplest calculator!");
+            Console.WriteLine("*********************************************");
+            Console.Write("Enter first number: ");
+            var nbr1AsStr = Console.ReadLine();
+            var nbr1 = Convert.ToInt32(nbr1AsStr);
+             
+            Console.Write("Enter second number: ");
+            var nbr2AsStr = Console.ReadLine();
+            var nbr2 = Convert.ToInt32(nbr2AsStr);
+
+            var answer = (nbr1 + nbr2);
+            Console.Write($"{nbr1} plus {nbr2} equals {answer}");
+
+
+            //Console.Write("Enter some data and press ENTER: ");
+            //var response = Console.ReadLine();
+            //Console.WriteLine(response);
+
+        }
+    }
+}
---
+++ HomeworkSolution1/HomeworkSolution1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HomeworkSolution1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Matt McKinney");
+            Console.WriteLine("matt.r.mck@gmail.com");
+            Console.WriteLine("(330) 720-6740");
+            Console.WriteLine("\t I am looking to work at a career which is both rewarding and challenging.");
+            Console.WriteLine("I want to have a skill that will give me more options and autonomy in my life.");
+        }
+    }
+}
