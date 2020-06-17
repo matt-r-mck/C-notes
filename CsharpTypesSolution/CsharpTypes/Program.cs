3a3da897b4f0c81c0f3565e5084ecd6406437a57
max-student <max-student@maxtrain.com>
2020-06-17 14:11:23 -0400

    Added if/return statement

--- CsharpTypesSolution/CsharpTypes/Program.cs
+++ CsharpTypesSolution/CsharpTypes/Program.cs
@@ -9,10 +9,20 @@ namespace CsharpTypes {
             Console.Write("Enter first number: ");
             var nbr1AsStr = Console.ReadLine();
             var nbr1 = Convert.ToInt32(nbr1AsStr);
-             
+            
+            if(nbr1 < 100 || nbr1 > 999) { 
+                Console.WriteLine("ERROR: You must enter a number between 100 and 999");
+                return;
+            }
+
             Console.Write("Enter second number: ");
             var nbr2AsStr = Console.ReadLine();
             var nbr2 = Convert.ToInt32(nbr2AsStr);
+
+            if (nbr2 > 1000) {
+                Console.WriteLine("ERROR: You must enter a number <= 1000");
+                return;
+            }
 
             var answer = (nbr1 + nbr2);
             Console.Write($"{nbr1} plus {nbr2} equals {answer}");
