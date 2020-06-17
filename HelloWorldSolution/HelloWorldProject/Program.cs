307a156a20cf0d224eef4ad2ed25f542acd9b765
max-student <max-student@maxtrain.com>
2020-06-17 11:45:06 -0400

    Initial Commit

---
+++ HelloWorldSolution/HelloWorldProject/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HelloWorldProject {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Console.WriteLine("by Matt McKinney");
+        }
+    }
+}
